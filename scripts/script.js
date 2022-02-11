70fec3431c2f0699f191599924ab0d615b083436
Daniel Schaeffer <daneschaeffer@hotmail.com>
2022-02-10 21:33:30 -0300

    att

--- scripts/script.js
+++ scripts/script.js
@@ -1,133 +1,130 @@
-function checkArrowShow(name1, name2, name3) {
-    const elementPosition = document.getElementById(name1);
-    const arrowElement = document.getElementById(name2);
-    const paragraphElement = document.getElementById(name3);
+function changeOptionStyle() {
 
-    elementPosition.addEventListener('click', function () {
-        elementPosition.classList.toggle('BGtransformation');
-        arrowElement.classList.toggle('visible');
-        paragraphElement.classList.toggle('colorChange');
-
-    }())
-}
-
-function checkArrowClickV2(name) {
-
-    const options = document.getElementById(name);
+    const options = document.getElementById('options');
     
     options.addEventListener('click', function(e) {
         const currentElement = e.target;
-        currentElement.classList.toggle('active');
-        currentElement.sibling.toggle('active');
-    })
-   
+        const nextBrother = currentElement.nextElementSibling;
+        const prevBrother = currentElement.previousElementSibling;
+
+        if (prevBrother != null && prevBrother.classList.contains('active')) {
+            prevBrother.classList.toggle('active')
+            currentElement.classList.toggle('active');
+        } else if(nextBrother != null && nextBrother.classList.contains('active')) {
+            nextBrother.classList.toggle('active')
+            currentElement.classList.toggle('active');
+        } else {
+            currentElement.classList.toggle('active');
+        }
+    }) 
 }
 
+function changeOtherOptionStyle() {
 
-function checkArrowClick(name) {
-    const classes = {
-        bgColor: 'BGtransformation',
-        visible: 'visible',
-        paint: 'colorChange'
-    }
-
-    const options = document.getElementById(name);
-    const arr = [];
-    let filterArr = [];
-    let idConvertString;
-    let secondIdConvertString;
+    const options = document.getElementById('optionsTwo');
     
     options.addEventListener('click', function(e) {
         const currentElement = e.target;
-        const id = currentElement.id.toString();
-        const position = document.getElementById(id)
-        const arrow = position.children[0].id.toString();
-        const paragraph = position.children[1].id.toString();
+        const nextBrother = currentElement.nextElementSibling;
+        const prevBrother = currentElement.previousElementSibling;
+        
+        currentElement.classList.toggle('active')
 
-        arr.push(id);
-
-        if(arr.length > 1) {
-            filterArr = arr.filter((id, i) => arr.indexOf(id) === i);
-            const foundElements = filterArr.find(element => element != id);
-            idConvertString = foundElements.toString();
-
-            const verifPosition = document.getElementById(idConvertString);
-            const verifArrow = verifPosition.children[0];
-            const verifParagraph = verifPosition.children[1];
-
-            if(filterArr.length > 1 && filterArr.length <= 2 && 
-                idConvertString != id) {
-
-                verifPosition.classList.remove(classes.bgColor);
-                verifArrow.classList.remove(classes.visible);
-                verifParagraph.classList.remove(classes.paint);
-
-                checkArrowShow(id, arrow, paragraph) 
-
-            } else if(filterArr.length > 2 &&  idConvertString != id) {
-
-                const foundMoreElements = filterArr.find(
-                element => element != id && element != foundElements);
-
-                secondIdConvertString = foundMoreElements.toString();
-                const verifOtherPosition = document.getElementById(secondIdConvertString);
-                const verifOtherArrow = verifOtherPosition.children[0];
-                const verifOtherParagraph = verifOtherPosition.children[1];
-
-                verifPosition.classList.remove(classes.bgColor);
-                verifArrow.classList.remove(classes.visible);
-                verifParagraph.classList.remove(classes.paint);
-
-                verifOtherPosition .classList.remove(classes.bgColor);
-                verifOtherArrow.classList.remove(classes.visible);
-                verifOtherParagraph.classList.remove(classes.paint);
-
-                checkArrowShow(id, arrow, paragraph) 
+        while(nextBrother) {
+            if(nextBrother.classList.contains('active')) {
+                nextBrother.classList.toggle('active');
             }
             
-        } else {
-                checkArrowShow(id, arrow, paragraph)
+            nextBrother = nextBrother.nextElementSibling;
         }
 
-    })
+        while(prevBrother) {
+            if(prevBrother.classList.contains('active')) {
+                prevBrother.classList.toggle('active');
+            }
+            
+            prevBrother = prevBrother.previousElementSibling;
+        }
+
+    }) 
+}
+
+function submitBtn() {
+    const btn = document.getElementById('submit');
+    const input = document.querySelectorAll('input');
+
    
 }
 
-function validateInput(input, title, error) {
-    let inputBox = document.getElementById(input);
-    let titleChange = document.getElementById(title)
-    let showErrorMessage = document.getElementById(error)
+
+function validateInput(input) {
+    const inputBox = document.getElementById(input);
+    const titleChange = inputBox.previousElementSibling;
+    const showErrorMessage = inputBox.nextElementSibling;
+    const submitBtn = document.getElementById('submit')
 
     inputBox.addEventListener('keyup', function() {
-        if(inputBox.value.length < 1) {
+
+        if(inputBox.value.length < 1 ) {
             titleChange.classList.remove('red')
             showErrorMessage.classList.remove('visible')
-        } else if(isNaN(parseFloat(inputBox.value))) {
+        } else if(isNaN(inputBox.value)) {
             titleChange.classList.add('red')
             showErrorMessage.classList.add('visible')
-        } 
-        else {
+        }  else {
             titleChange.classList.remove('red')
         }
     })
+
+}
+
+function checkTab() {
+    const inputs =  document.querySelectorAll('input');
+    
+    inputs.forEach(input => input.addEventListener('keyup', function(e) {
+
+        if(e.key == 'Tab') {
+            const currentElement = e.target
+            const id = currentElement.id.toString();
+            validateInput(id)
+        }
+    }))
 }
 
 function executeInput(name) {
-    
-    const calculatorPosition = document.getElementById(name)
+    const calculatorPosition = document.getElementById(name);
 
     calculatorPosition.addEventListener('click', function(e) {
         const currentElement = e.target;
-        const id = currentElement.id.toString();
-        const inputPosition = document.getElementById(id);
-        const titlePosition = inputPosition.previousElementSibling.id;
-        const errorMessage = inputPosition.nextElementSibling.id;
-        
-        validateInput(id, titlePosition, errorMessage)
+        const id = currentElement.id;
+        checkTab()
+        validateInput(id)
+    })
+
+}
+
+function cleanButton() {
+    const btn = document.getElementById('clear')
+
+    btn.addEventListener('click', function() {
+        const redClasses = document.querySelectorAll('.red');
+        const inputs = document.querySelectorAll('.textStyle');
+        const isVisible = document.querySelectorAll('.visible');
+        const isActive = document.querySelectorAll('.active');
+        const submitBtn = document.getElementById('submit');
+
+        redClasses.forEach(redClass => redClass.classList.remove('red'));
+        inputs.forEach(input => input.value = '');
+        isVisible.forEach(errorMsg => errorMsg.classList.remove('visible'));
+        isActive.forEach(activeItems => activeItems.classList.toggle('active'));
+        submitBtn.classList.remove('submit-active')
     })
 }
 
+changeOptionStyle()
+changeOtherOptionStyle()
 executeInput('findInput')
 executeInput('findInput2')
-checkArrowClickV2('options')
-checkArrowClick('optionsTwo')
+checkTab()
+cleanButton()
+submitBtn()
